ce3cf05343fea8c9fba932691e4740e954625f56
Daruka Roshan RajKumar <roshandaruka@Darukas-MacBook-Pro.local>
2017-05-29 21:57:13 +0530

    github page configuration

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import throttle from "lodash/throttle";
 import { store } from "./store.js";
 import { router } from "./router.js";
 
+import './stylesheets/main.js';
+
 // render the main component
 ReactDOM.render(
   <Provider store={store}>
